ea3435d24c64d9ed899366bb5bae61b4984a653a
FrankJanePartner <partnermarvel55@gmail.com>
2025-08-26 06:17:20 +0100

    modified:   automations/apps.py

--- automations/apps.py
+++ automations/apps.py
@@ -3,14 +3,7 @@ from django.apps import AppConfig
 
 class AutomationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<< HEAD:automations/apps.py
     name = 'automations'
     
     def ready(self):
         import automations.signals
-=======
-    name = 'apps.automations'
-    
-    def ready(self):
-        import apps.automations.signals
->>>>>>> f67fec8c536cd79bdb7a2eeba72fda701318a54d:apps/automations/apps.py
