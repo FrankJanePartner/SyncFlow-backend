3f786ccfeef50b06ab2606ca58bacd7829e570db
FrankJanePartner <partnermarvel55@gmail.com>
2025-09-05 11:07:12 +0100

    modified:   UserAccount/urls.py

--- UserAccount/urls.py
+++ UserAccount/urls.py
@@ -6,6 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path("register/", apis.RegisterApi.as_view(), name="register"),
     path("login/", apis.LoginApi.as_view(), name="login"),
-    path("me/", apis.UserApi.as_view(), name="me"),
+    path("user/", apis.UserApi.as_view(), name="user"),
     path("logout/", apis.LogoutApi.as_view(), name="logout"),
 ]
