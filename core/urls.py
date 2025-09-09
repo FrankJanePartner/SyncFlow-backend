4c18992872b0bd5d3a94a6ef395efd3d25a33ffe
FrankJanePartner <partnermarvel55@gmail.com>
2025-09-09 16:36:07 +0100

    root directory correctted

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from .views import BrandListCreateView, BrandDetailView, guest_demo
 urlpatterns = [
     path('brands/', BrandListCreateView.as_view(), name='brand-list'),
     path('brands/<int:pk>/', BrandDetailView.as_view(), name='brand-detail'),
-    path('guest_demo', guest_demo, name="guest_demo")
+    path('guest_', guest_demo, name="guest")
 ]
