25db78b826a98b9eb74a939dd4c2483ce06c4b62
FrankJanePartner <partnermarvel55@gmail.com>
2025-09-10 13:19:48 +0100

    modified:   core/admin.py modified:   social/admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Brand
+from social.models import SocialAccount
+
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
@@ -7,3 +9,14 @@ class BrandAdmin(admin.ModelAdmin):
     list_filter = ['created_at']
     search_fields = ['name', 'user__email']
     raw_id_fields = ['user']
+
+
+
+@admin.register(SocialAccount)
+class SocialAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'platform', 'username', 'is_active', 'created_at')
+    list_filter = ('platform', 'is_active')
+    search_fields = ('user__email', 'username')
+    raw_id_fields = ('user',)
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
